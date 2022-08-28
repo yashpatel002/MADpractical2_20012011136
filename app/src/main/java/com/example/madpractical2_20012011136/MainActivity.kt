5adbc2df0f0b52a7da1b9bc148e0f0908fbaca3b
Yash Patel <ympatel132635@gmail.com>
2022-08-28 21:24:55 +0530

    updated practical2

--- app/src/main/java/com/example/madpractical2_20012011136/MainActivity.kt
+++ app/src/main/java/com/example/madpractical2_20012011136/MainActivity.kt
@@ -2,10 +2,51 @@ package com.example.madpractical2_20012011136
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
+    val TAG="MainActivity"
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        showMessage("onCreate function called.")
+    }
+
+    fun showMessage(msg:String){
+        Log.i(TAG,msg)
+        Toast.makeText(this,msg,Toast.LENGTH_SHORT).show()
+        Snackbar.make(findViewById(R.id.mymsg),msg,Snackbar.LENGTH_SHORT).show()
+    }
+
+    override fun onStart() {
+        showMessage("onStart function called.")
+        super.onStart()
+    }
+
+    override fun onResume() {
+        showMessage("onResume function called.")
+        super.onResume()
+    }
+
+    override fun onRestart() {
+        showMessage("onRestart function called.")
+        super.onRestart()
+    }
+
+    override fun onPause() {
+        showMessage("onPause function called.")
+        super.onPause()
+    }
+
+    override fun onDestroy() {
+        showMessage("onDestroy function called.")
+        super.onDestroy()
+    }
+
+    override fun onStop() {
+        showMessage("onStop function called.")
+        super.onStop()
     }
 }
